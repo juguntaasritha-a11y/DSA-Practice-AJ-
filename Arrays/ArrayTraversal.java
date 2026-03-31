5b62a1803cfe3efed55b52a3c2b658aa256a8ef7
juguntaasritha-a11y <juguntaasritha@gmail.com>
2026-03-31 22:04:39 +0530

    Day 1: Array Traversal

---
+++ Arrays/ArrayTraversal.java
@@ -0,0 +1,14 @@
+//Day-1 Array Traversal
+// Time Complexity: O(n)
+// space complexity: O(1)
+
+class Main{
+    public static void main(String[]args){
+        int[] arr = {10,20,30,40,50};
+        System.out.println("Array elements are:");
+        
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
